7299702ca1730ec79ca445fe190107dfbf285724
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-01-02 01:30:10 -0500

    added terminal

--- app/components/app.component.js
+++ app/components/app.component.js
@@ -1,12 +1,23 @@
 (function(app) {
+
   app.AppComponent = ng.core
     .Component({
       selector: 'wilfried',
-      templateUrl: '/app/views/wilfried.html'
+      templateUrl: '/app/views/wilfried.html',
+      directives: [ng.common.NgSwitch, ng.common.NgSwitchWhen, ng.common.NgSwitchDefault, app.Terminal]
     })
     .Class({
       constructor: function() {
-      	this.timer = 5;
-      }
+      	this.resource = window.location.pathname.split("/").splice(1);
+      	this.startup();
+      },
+      getResource: function () {
+      	return this.resource[0].toLowerCase();
+      },
+      startup: function() {
+      	if (this.getResource() != "terminal") {
+      		window.location.pathname = "/w/";
+      	};
+  		}
     });
 })(window.app || (window.app = {}));
